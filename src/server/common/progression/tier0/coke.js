9779629e8595f7a0fcb8440cfcc205fa5a3b181f
Lura Skye <l@veriny.tf>
2023-10-20 16:27:37 +0100

    GET JEMI'D

--- src/server/common/progression/tier0/coke.js
+++ src/server/common/progression/tier0/coke.js
@@ -1,5 +1,8 @@
 import { RecipeWrapper } from "../../../../util/recipe_wrapper";
 
+const BASE_COKE_TIME = 1200;
+const BLOCK_COKE_TIME = BASE_COKE_TIME * 8;  // slightly more efficient to smelt whole coal blocks.
+
 /** @param {RecipeWrapper} event */
 export const readdCokeOvenRecipes = (event) => {
     // no vanilla charcoal recipe
@@ -8,8 +11,18 @@ export const readdCokeOvenRecipes = (event) => {
     event.kjs.recipes.gtceu.coke_oven("nijika:tier0/coke_oven/charcoal_cooking")
         .itemInputs("1x minecraft:oak_log")
         .itemOutputs("minecraft:charcoal")
-        // .outputFluids(Fluid.of("gtceu:creosote", 250 * FluidAmounts.MB))
-        .duration(1200);
+        .outputFluids(Fluid.of("gtceu:creosote", 250 * FluidAmounts.MB))
+        .duration(BASE_COKE_TIME);
     
-    
+    event.kjs.recipes.gtceu.coke_oven("nijika:tier0/coke_oven/coke_cooking")
+        .itemInputs("1x #c:coal_gems")
+        .itemOutputs("gtceu:coke_gem")
+        .outputFluids(Fluid.of("gtceu:creosote", 500 * FluidAmounts.MB))
+        .duration(BASE_COKE_TIME);
+
+    event.kjs.recipes.gtceu.coke_oven("nijika:tier0/coke_oven/coke_block_cooking")
+        .itemInputs("1x #c:coal_blocks")
+        .itemOutputs("8x gtceu:coke_block")
+        .outputFluids(Fluid.of("gtceu:creosote", 500 * 9 * FluidAmounts.MB))
+        .duration(BLOCK_COKE_TIME);
 }
